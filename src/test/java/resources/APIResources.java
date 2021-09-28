1a969bd0eaf17fb0a2305da5cafec00bba589510
Diego Pereira Grassato <diego.grassato@bradesco.com.br>
2021-09-28 16:33:37 -0300

    Ajusta estrutura

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -3,7 +3,9 @@ package resources;
 public enum APIResources {
     AddPlaceAPI("/maps/api/place/add/json"),
     DeletePlaceAPI("/maps/api/place/delete/json"),
-    viaCEP("/ws/12246870/json");
+    viaCEP("/ws/12246870/json"),
+    viaCEP1("/ws/12246870/json"),
+    viaCEP2("/ws/12246873/json");
     private final String resource;
 
     APIResources(final String resource) {
